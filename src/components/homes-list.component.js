75950a1d7a2a6f83087843a0f9360927a5f90dd5
Angela-Mccleery-Miller <angemam@gmail.com>
2020-05-10 22:25:04 -0600

    removed images from Inventory page

--- src/components/homes-list.component.js
+++ src/components/homes-list.component.js
@@ -12,9 +12,9 @@ const Home = props => (
         <td className={props.home.completed ? 'EDITED' : ''}>{props.home.bedrooms}</td>
         <td className={props.home.completed ? 'EDITED' : ''}>{props.home.bathrooms}</td>
         <td className={props.home.completed ? 'EDITED' : ''}>{props.home.sqfeet}</td>
-        <td className={props.home.completed ? 'EDITED' : ''}>{props.home.url}</td>
+        {/* <td className={props.home.completed ? 'EDITED' : ''}>{props.home.url}</td>
         <td className={props.home.completed ? 'EDITED' : ''}>{props.home.fp1_url}</td>
-        <td className={props.home.completed ? 'EDITED' : ''}>{props.home.fp2_url}</td>
+        <td className={props.home.completed ? 'EDITED' : ''}>{props.home.fp2_url}</td> */}
 
 
         <td>
@@ -72,9 +72,9 @@ homeList() {
                             <th>BEDROOMS</th>
                             <th>BATHROOMS</th>
                             <th>SQUARE FEET</th>
-                            <th>EXT IMG</th>
+                            {/* <th>EXT IMG</th>
                             <th>FP 1</th>
-                            <th>FP 2</th>
+                            <th>FP 2</th> */}
                         </tr>
                     </thead>
                     <tbody>
